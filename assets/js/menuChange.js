fd544a27a2329d81908a6f2452a8249f7996356d
Steve Barry <nbs5000.dev@gmail.com>
2024-02-23 11:30:13 +1100

    menu and navbar added and amended

--- assets/js/menuChange.js
+++ assets/js/menuChange.js
@@ -2,7 +2,20 @@
 
 function burgerMenu(x) {
     x.classList.toggle("change");
+    toggle_change("theMenu","c","change");
 }
 function wordMenu(x) {
     x.classList.toggle("change");
+    toggle_change("theMenu","c","change");
+}
+function toggle_change(elToFind,elType,classToToggle){
+    let arr=[];
+    const z = elType=="c"?document.getElementsByClassName(elToFind):document.getElementById(elToFind);
+    arr=elType=="c"?z:[z];
+    const len = elType=="c"?arr.length:1;
+    let i=0;
+    while(i<len){
+        arr[i].classList.toggle(classToToggle);
+        i++
+    };
 }
--- assets/js/stickyScroll.js
+++ assets/js/stickyScroll.js
@@ -6,11 +6,13 @@ function scrollFunction() {
     const navbar = document.getElementById("navbar");
     const navbg = document.getElementById("navBg");
     const sticky = navbar.offsetTop;
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky","shadow");
-        navbg.classList.add("stickyBg");
-    } else {
-        navbar.classList.remove("sticky","shadow");
-        navbg.classList.remove("stickyBg");
+    if (window.pageYOffset >= sticky && !navbar.classList.contains("sticky")) {
+        toggle_change("navbar","i","sticky");
+        toggle_change("navbar","i","shadow");
+        toggle_change("navBg","i","stickyBg");
+    } else if(window.pageYOffset <= sticky) {
+        toggle_change("navbar","i","sticky");
+        toggle_change("navbar","i","shadow");
+        toggle_change("navBg","i","stickyBg");
     }
 }
