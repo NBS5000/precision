78a2ca66b896f4000be0181ea0bea98acf5c1020
Steve Barry <nbs5000.dev@gmail.com>
2024-04-28 22:43:15 +1000

    video added, video bg to be part of v2

---
+++ pages/modals/modal_pages.js
@@ -0,0 +1,17 @@
+function generateModal(type,content){
+    switch (type) {
+        case contact:
+            gen_contact();
+            break;
+    
+        default:
+            break;
+    }
+}
+function gen_contact(){
+    document.getElementById("modal");
+
+    const form = ``;
+
+
+}
